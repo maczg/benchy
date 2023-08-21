7eeb64f4be2dea9feb96f94b47e759503bf6642e
massimo-gollo <massi.gollo@gmail.com>
2023-08-21 20:52:49 +0200

    add sample server with trace exporter

---
+++ cmd/trace_server.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/massimo-gollo/benchy/pkg/log"
+	"github.com/massimo-gollo/benchy/pkg/mixin"
+	"github.com/massimo-gollo/benchy/pkg/trace"
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/otel"
+	tr "go.opentelemetry.io/otel/trace"
+	"net/http"
+	"time"
+)
+
+var traceSrvCmd = cobra.Command{
+	Use:   "server",
+	Short: "Start simple server with tracing",
+	Long:  "Start simple server instrumented with otel for tracing",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		logger := log.NewDefaultLogger()
+		logger.Infoln("starting server: init tracing")
+
+		var otlpEndpoint = mixin.GetEnvOrDefault("OTLP_ENDPOINT", "localhost:4317")
+
+		ctx := context.Background()
+
+		shutdown, err := trace.InitOLTPTracer("server", otlpEndpoint)
+		if err != nil {
+			logger.WithError(err).Fatalln("failed to initialize tracer")
+		}
+		defer func() {
+			if err := shutdown(ctx); err != nil {
+				logger.WithError(err).Errorln("failed to shutdown tracer")
+			}
+		}()
+
+		tracer := otel.Tracer("server")
+
+		r := mux.NewRouter()
+		r.Use(trace.Middleware(tracer, log.SetLogName("tracer", logger)))
+
+		r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("ok\n"))
+		})
+
+		r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx, span := tracer.Start(
+				ctx,
+				"work-test-handler")
+			defer span.End()
+
+			doWork(ctx, tracer)
+
+			// emulate latency work
+			time.Sleep(100 * time.Millisecond)
+
+			_, _ = w.Write([]byte("test-handler\n"))
+		})
+
+		logger.Infoln("starting server: listening on :8080")
+		logger.Fatalln(http.ListenAndServe(":8080", r))
+
+	},
+}
+
+func doWork(ctx context.Context, tracer tr.Tracer) {
+	ctx, span := tracer.Start(
+		ctx,
+		"work-test-handler-1")
+	defer span.End()
+	// emulate doing work
+	time.Sleep(200 * time.Millisecond)
+}
--- go.mod
+++ go.mod
@@ -7,11 +7,30 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.7.0
+	go.opentelemetry.io/otel v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
+	go.opentelemetry.io/otel/trace v1.16.0
+	google.golang.org/grpc v1.57.0
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230526161137-0005af68ea54 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230525234035-dd9d682886f9 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
